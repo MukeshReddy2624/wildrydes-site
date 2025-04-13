152054e34b3ae5541e14aa91db661ee64ec3e61f
MukeshReddy2624 <162849697+MukeshReddy2624@users.noreply.github.com>
2025-04-13 17:40:47 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_9bkmGeWXD', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '23jkal8585cq1uct5gvci7st4c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_speMfctIG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '65s4e891667rr87mqpqfc7cg0l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
